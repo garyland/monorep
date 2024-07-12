70f22142c24403471bea9cf4d929472d88d978e1
Gary Land <gary.land@civica.co.uk>
2024-07-12 10:36:33 +0100

    Output env

--- G2/server.js
+++ G2/server.js
@@ -4,13 +4,26 @@
 function main(params) {
     console.log('');
     console.log('*********************************************************');
+    console.log('');
     console.log('Hello')
     console.log('Test');
     console.log('This is on the test branch')
 
-    console.log(JSON.stringify(params);
-    
+    console.log('');
+    console.log('PARAMS');
+    console.log(JSON.stringify(params, null, 2));
+
+    console.log('');
+    console.log('ENV');
+    console.log(JSON.stringify(process.env, null, 2));
+
+    console.log('');
+    console.log('*********************************************************');
+    console.log('');
+
     // const base = new Base(params, params.pickArguments);
 	// return base.request(pick(params, ['body', 'path', 'method']));
 
 }
+
+main(process.argv);
