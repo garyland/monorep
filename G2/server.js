fbe386dadcf26ca98c923fb4f0b7e275a9cfab45
Gary Land <gary.land@civica.co.uk>
2024-07-12 10:26:20 +0100

    Move to main

--- G2/server.js
+++ G2/server.js
@@ -1,5 +1,16 @@
-console.log('');
-console.log('*********************************************************');
-console.log('Hello')
-console.log('Test');
-console.log('This is on the test branch')
+
+
+
+function main(params) {
+    console.log('');
+    console.log('*********************************************************');
+    console.log('Hello')
+    console.log('Test');
+    console.log('This is on the test branch')
+
+    console.log(JSON.stringify(params);
+    
+    // const base = new Base(params, params.pickArguments);
+	// return base.request(pick(params, ['body', 'path', 'method']));
+
+}
